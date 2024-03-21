387d039d8178f41ef33aeddb29df77b86d7cd00c
Wojciech Zawadzki <zawadzki1993@hotmail.com>
2024-03-21 18:02:49 +0100

    Add project files.

---
+++ HomeWork4Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		// Declaration of variables
+		int number1 = 10;
+		string text = "Szkoła Dotneta";
+		double number2 = 12.5;
+
+		// Displaying variable values
+		Console.WriteLine("Value of variable 'number1': " + number1);
+		Console.WriteLine("Value of variable 'text': " + text);
+		Console.WriteLine("Value of variable 'number2': " + number2);
+
+		Console.ReadKey();
+	}
+}
